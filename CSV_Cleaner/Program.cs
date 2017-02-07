ef22bd23cba7b7321a8f7d33d4cae76497ab5907
Cody <Cody@CODY>
2017-02-07 23:58:49 +0800

    Add project files.

---
+++ CSV_Cleaner/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSV_Cleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Finder find = new Finder();
+            //Read database
+            string database = File.ReadAllText("Member Database.csv");
+            //Split into members
+            string[] member = database.Split('\n');
+            string[] details;
+            //Remaining Values
+            string first_name, surname, email;
+
+            //Delete previous cleaner
+            if (File.Exists("Clean Database.csv"))
+            {
+                File.Delete("Clean Database.csv");
+            }
+                        
+            using (FileStream fs = File.Create("Clean Database.csv"))
+            {
+                //For each member
+                for (int i = 0; i < member.Length; i++)
+                {
+                    details = member[i].Split(',');
+                    first_name = details[2];
+                    surname = details[3];
+                    email = find.findEmail(details);
+                    if(email == "" || email == "Plantsplus@clyx.com.au" || email == "plantsplus@clyx.com.au")
+                    {
+                        continue;
+                    }
+                    byte[] memberInfo = new UTF8Encoding(true).GetBytes(first_name+","+surname+","+email+"\n");
+                    fs.Write(memberInfo, 0, memberInfo.Length);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSV_Cleaner/finder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSV_Cleaner
+{
+    class Finder
+    {
+        public string findEmail(string[] details)
+        {
+            for(int i = 0; i<details.Length; i++)
+            {
+                if(details[i].Contains("@"))
+                {
+                    return details[i];
+                }
+            }
+            return "";
+        }
+    }
+}
